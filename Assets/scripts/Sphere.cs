f4cdc2a86a49fff4bf65ac9623af41f62a0ee7fb
Krasnoshchekova <44660984+Krasnoshchekova@users.noreply.github.com>
2019-10-20 21:47:14 +0500

    вставка без папки library

---
+++ Assets/scripts/Sphere.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sphere : MonoBehaviour
+{
+	Rigidbody rb;
+	public float A = 0.01F;
+	public float k = 0.05F;
+	float Omega;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+		ls ();
+    }
+
+	void ls(){
+		Omega = Mathf.Sqrt (k / rb.mass);
+	}
+
+    void Update()
+    {
+		rb.MovePosition( new Vector3( 0f, A*Mathf.Cos(Omega*Time.realtimeSinceStartup), 0f) );
+    }
+}
