c91499159d9b29db4006687f9392771479c53e35
Михаил Кузнецов <kuznetsOFFmikhail@outlook.com>
2019-12-06 11:37:26 +0500

    Большее количество тел
    Для 2х и 3х тел

---
+++ Point around point/Assets/Scripts/cube_pos.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube_pos : MonoBehaviour {
+	
+	Rigidbody rb;
+	public Rigidbody rb1;
+	public Vector3 r;
+	public Vector3 R;
+	public Vector3 V;
+	float g = 6.67408e-2F;
+	public float m;
+	public float M;
+
+
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.position= r;
+		rb.velocity = V;
+
+		Debug.Log (rb.position);
+	}
+
+	void Update ()
+	{
+		
+		r = rb.position-rb1.position;
+		R = rb.position;
+		float l = -g * m / Mathf.Pow (r.magnitude, 3);
+		float L = -g * M / Mathf.Pow (R.magnitude, 3);
+		rb.AddForce (l*r+L*R);
+		Debug.Log (l*r);
+
+	}
+}
